28a4432cdb3e02a8712cf9b95836516263722de4
alvinfrancis <alvinfrancisdumalus@gmail.com>
2014-07-30 01:09:37 +0800

    More ensime setup

--- init_ensime.el
+++ init_ensime.el
@@ -1,19 +1,21 @@
-(defvar ensime-slickdoc-url-base
-  "http://slick.typesafe.com/doc/2.0.0-M3/api/index.html#"
-  "URL base for constructing slick links.")
+(defun setup-ensime ()
+  (defvar ensime-slick-prefix "^scala\\.slick\\.")
 
-(defun ensime-make-slick-doc-url-helper
-  (url-base type &optional member)
-  "Given a scala type, and optionally a type member, construct the
+  (defvar ensime-slickdoc-url-base
+    "http://slick.typesafe.com/doc/2.0.0-M3/api/index.html#"
+    "URL base for constructing slick links.")
+
+  (defun ensime-make-slick-doc-url-helper
+    (url-base type &optional member)
+    "Given a scala type, and optionally a type member, construct the
    corresponding slick url.  Currently does not narrow down to member"
-  (concat url-base (ensime-type-full-name type)))
+    (concat url-base (ensime-type-full-name type)))
 
-(defun ensime-make-slick-doc-url (type &optional member)
-  (ensime-make-slick-doc-url-helper
-   ensime-slickdoc-url-base type member))
+  (defun ensime-make-slick-doc-url (type &optional member)
+    (ensime-make-slick-doc-url-helper
+     ensime-slickdoc-url-base type member))
 
-(let ((slick-prefix "^scala\\.slick\\." ))
-  ;; Add slick doc lookup to lookup-map
-  (when (assoc slick-prefix ensime-doc-lookup-map)
-    (defvar ensime-doc-lookup-map
-      (cons '("^scala\\.slick\\." . ensime-make-slick-doc-url) ensime-doc-lookup-map))))
+  (unless (assoc ensime-slick-prefix ensime-doc-lookup-map)
+    (add-to-list 'ensime-doc-lookup-map `(,ensime-slick-prefix . ensime-make-slick-doc-url))))
+
+(add-hook 'ensime-connected-hook 'setup-ensime)
