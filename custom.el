9420a40402e448dc2215652a858c878465d796fc
alvinfrancis <alvinfrancisdumalus@gmail.com>
2015-01-22 20:06:18 +0800

    Update custom-set-faces

--- custom.el
+++ custom.el
@@ -3,11 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo"))))
  '(elscreen-tab-background-face ((t (:inherit default))))
  '(elscreen-tab-current-screen-face ((t (:height 0.8))))
  '(elscreen-tab-other-screen-face ((t (:inherit linum))))
- '(magit-item-highlight ((t nil))))
+ '(magit-item-highlight ((t nil)))
+ '(fringe ((t (:inherit default :background nil))))
+ '(linum ((t (:inherit default :box nil :height 0.8 :background nil)))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
