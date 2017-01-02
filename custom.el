8bc2b8f7865c670a784898270e5a7e95be9b6f9d
alvinfrancis <alvinfrancisdumalus@gmail.com>
2017-01-02 16:35:21 +0800

    Include default face custom

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :weight normal :height 110 :width normal :family "Monoid"))))
  '(elscreen-tab-background-face ((t (:inherit default))))
  '(elscreen-tab-current-screen-face ((t (:height 0.8))))
  '(elscreen-tab-other-screen-face ((t (:inherit linum))))
